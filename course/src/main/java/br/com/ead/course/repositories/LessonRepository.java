da8207d89313f3bc09a77c14a42a5e5699461511
Wallace Gonçalves <wallacecosme@gmail.com>
2022-04-22 22:38:15 -0300

    Add a anotação @Repository nos repositorios

--- course/src/main/java/br/com/ead/course/repositories/LessonRepository.java
+++ course/src/main/java/br/com/ead/course/repositories/LessonRepository.java
@@ -3,9 +3,11 @@ package br.com.ead.course.repositories;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import br.com.ead.course.models.Lesson;
 
+@Repository
 public interface LessonRepository extends JpaRepository<Lesson, UUID> {
 
 }
--- course/src/main/java/br/com/ead/course/repositories/ModuleRepository.java
+++ course/src/main/java/br/com/ead/course/repositories/ModuleRepository.java
@@ -3,7 +3,11 @@ package br.com.ead.course.repositories;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import br.com.ead.course.models.Module;
+
+@Repository
 public interface ModuleRepository extends JpaRepository<Module, UUID> {
 
 }
