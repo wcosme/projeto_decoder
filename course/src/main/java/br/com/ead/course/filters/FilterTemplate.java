a519953929f57955baad3ec208f0e03582965a8a
Wallace Gonçalves <wallacecosme@gmail.com>
2022-04-25 19:27:38 -0300

    Refatoração dos métodos do FilterTemplate

--- course/src/main/java/br/com/ead/course/filters/FilterTemplate.java
+++ course/src/main/java/br/com/ead/course/filters/FilterTemplate.java
@@ -1,9 +1,7 @@
 package br.com.ead.course.filters;
 
-import java.util.Collection;
 import java.util.UUID;
 
-import javax.persistence.criteria.Expression;
 import javax.persistence.criteria.Root;
 
 import org.springframework.data.jpa.domain.Specification;
@@ -17,41 +15,34 @@ import net.kaczmarzyk.spring.data.jpa.web.annotation.And;
 import net.kaczmarzyk.spring.data.jpa.web.annotation.Spec;
 
 public class FilterTemplate {
+
+	@And({ @Spec(path = "courseStatus", spec = Equal.class), @Spec(path = "courseLevel", spec = Equal.class),
+			@Spec(path = "name", spec = LikeIgnoreCase.class) })
+	public interface CourseFilter extends Specification<Course> {
+	}
+
+	@Spec(path = "title", spec = LikeIgnoreCase.class)
+	public interface ModuleFilter extends Specification<Module> {
+	}
+
+	@Spec(path = "title", spec = LikeIgnoreCase.class)
+	public interface LessonFilter extends Specification<Lesson> {
+	}
+
+	public static Specification<Module> moduleCourseId(final UUID courseId) {
+		return (root, query, cb) -> {
+			query.distinct(true);
+			Root<Module> module = root;			
+			return cb.and(cb.equal(module.get("course").get("courseId"), courseId));
+		};
+	}
+
 	
-	@And({
-		@Spec(path = "courseStatus", spec = Equal.class),
-		@Spec(path = "courseLevel", spec = Equal.class),
-		@Spec(path = "name", spec = LikeIgnoreCase.class)
-	})
-	public interface CourseFilter extends Specification<Course>{}
-	
-	
-	@Spec(path = "title", spec = LikeIgnoreCase.class)	
-	public interface ModuleFilter extends Specification<Module>{}
-	
-		
-	@Spec(path = "title", spec = LikeIgnoreCase.class)	
-	public interface LessonFilter extends Specification<Lesson>{}
-	
-		
-    public static Specification<Module> moduleCourseId(final UUID courseId) {
-        return (root, query, cb) -> {
-            query.distinct(true);
-            Root<Module> module = root;
-            Root<Course> course = query.from(Course.class);
-            Expression<Collection<Module>> courseModules = course.get("modules");
-            return cb.and(cb.equal(course.get("courseId"), courseId), cb.isMember(module, courseModules));
-        };
-    }
-    
-    
-    public static Specification<Lesson> lessonModuleId(final UUID moduleId) {
-        return (root, query, cb) -> {
-            query.distinct(true);
-            Root<Lesson> lesson = root;
-            Root<Module> module = query.from(Module.class);
-            Expression<Collection<Lesson>> moduleLessons = module.get("lessons");
-            return cb.and(cb.equal(module.get("moduleId"), moduleId), cb.isMember(lesson, moduleLessons));
-        };
-    }
+	public static Specification<Lesson> lessonModuleId(final UUID moduleId) {
+    	return (root, query, cb) -> {
+    		query.distinct(true);
+    		Root<Lesson> lesson = root;
+    		return cb.and(cb.equal(lesson.get("module").get("moduleId"), moduleId));
+    	};
+	}
 }
