4bfac6ae833947cc8704633df887d580afbc7d52
Wallace Gonçalves <wallacecosme@gmail.com>
2022-04-25 21:33:43 -0300

    Implementando Logging nos Microservices com Log4J2

--- authuser/src/main/java/br/com/ead/authuser/controllers/AuthenticationController.java
+++ authuser/src/main/java/br/com/ead/authuser/controllers/AuthenticationController.java
@@ -1,55 +1,59 @@
 package br.com.ead.authuser.controllers;
 
-import br.com.ead.authuser.dtos.UserDto;
-import br.com.ead.authuser.enums.UserStatus;
-import br.com.ead.authuser.enums.UserType;
-import br.com.ead.authuser.models.UserModel;
-import br.com.ead.authuser.services.UserService;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.fasterxml.jackson.annotation.JsonView;
 
-import java.time.LocalDateTime;
-import java.time.ZoneId;
+import br.com.ead.authuser.dtos.UserDto;
+import br.com.ead.authuser.enums.UserStatus;
+import br.com.ead.authuser.enums.UserType;
+import br.com.ead.authuser.models.UserModel;
+import br.com.ead.authuser.services.UserService;
+import lombok.extern.log4j.Log4j2;
 
+@Log4j2
 @RestController
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RequestMapping("/auth")
 public class AuthenticationController {
-	
-	Logger logger = LoggerFactory.getLogger(AuthenticationController.class);
 
     @Autowired
     private UserService userService;
 
     @PostMapping("/signup")
     public ResponseEntity<Object> registerUser(@RequestBody @Validated(UserDto.UserView.RegistrationPost.class)
-    										   @JsonView(UserDto.UserView.RegistrationPost.class) UserDto dto){
+    										   @JsonView(UserDto.UserView.RegistrationPost.class) UserDto userDto){
 
-    	logger.info("Validando se o usuário passado existe");
-        if(userService.existsByUserName(dto.getUserName())){        	
+    	log.debug("POST registerUser userDto {}", userDto.toString());
+        if(userService.existsByUserName(userDto.getUserName())){        	
             return ResponseEntity.status(HttpStatus.CONFLICT).body("Error: UserName is Already Taken!");
         }
-        if(userService.existsByEmail(dto.getEmail())){
+        if(userService.existsByEmail(userDto.getEmail())){
             return ResponseEntity.status(HttpStatus.CONFLICT).body("Error: Email is Already Taken!");
         }
 
         var userModel = new UserModel();
-        BeanUtils.copyProperties(dto, userModel);
+        BeanUtils.copyProperties(userDto, userModel);
         userModel.setUserStatus(UserStatus.ACTIVE);
         userModel.setUserType(UserType.STUDENT);
         userModel.setCreationDate(LocalDateTime.now(ZoneId.of("UTC")));
         userModel.setLastUpdateDate(LocalDateTime.now(ZoneId.of("UTC")));
 
         userService.save(userModel);
+        log.debug("POST registerUser userDto save {}", userModel.toString());
+        log.info("User saved successfully userId {}", userModel.getUserId());
 
         return ResponseEntity.status(HttpStatus.CREATED).body(userModel);
     }
